2b04406e57bd044d4fcdf042e9e80276696f5442
Garza <igarza8@cps.k12.il.us>
2024-09-18 15:10:50 -0500

    first commit

---
+++ python_learning/python_basics/lesson_1.py
@@ -0,0 +1,32 @@
+#mkdir means make directory
+#cd means chnage directory
+#ls means list directory
+#pwd means print working directory 
+#touch means create a file
+#rm means remove a file
+#pound sign means comment
+# print("Hello,World")
+# print("itzel")
+# print("age \t 16")
+# # \t means tab
+# print("address")
+# press control ? to comment out muptiple lines
+
+# ask for input
+name = input("What is your name? ")
+print("hello,"+name)
+# ask for age
+
+age=input("how old are you?")
+print("you are" + age+"years old")
+
+Orgin=input("Place of birth")
+print("you are from!"+Orgin)
+
+home=input("whats your address?")
+print("Your address is" + home)
+
+school=input ("what school did you attend?")
+print("you attended"+school)
+
+print("hello,"+name +" you are" +age +"years old"+"you are from!"+Orgin+"whats your address"+"Your address is" + home +"what school did you attend?" +"you attended"+school)
