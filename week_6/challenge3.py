39bc9b347a2cf6346bf8045624305f2faf6ed441
Garza <igarza8@cps.k12.il.us>
2024-10-23 15:03:52 -0500

    first commit

--- week_6/challenge3.py
+++ week_6/challenge3.py
@@ -1,39 +1,51 @@
-#1 ask peice of text
-userText=input("enter any text")
-#2ask for 3 letters
-onesletter=input("1st letter")
-secondletter=input("2st letter")
-threeletter=input("3st letter")
+#Emily Wawrzyniak, Yao Hua He, Itzel Garza, Emily Hernandez
+#1. ask for peice of text 
+userText = input(" Enter any text you please:")
+#2. ask for 3 letter
+firstLetter =input(" Enter the first letter : ")
+secondLetter = input('Now enter the second letter: ')
+thirdLetter =input('Finally, enter the last letter')
 
-#string to lowercase
-onesletter.lower()
-secondletter.lower()
-threeletter.lower()
+letterValue = [firstLetter, secondLetter, thirdLetter]
+#3. convert string to lowercase
 userText.lower()
+letters= [firstLetter.lower(), secondLetter.lower(), thirdLetter.lower()]
 
-#string to list
-listofwords=userText.split()
-print(listofwords)
+#4. Convert string to list 
+listofWords = userText.split()
 
-#print wordcount
-wordcount=len(listofwords)
-print(f"the word count is{wordcount}")
+#5. print out word count (how many objects are in list 
+wordCount= len(listofWords)
+print(f"The word count is {wordCount} words")
 
-#invert list of words
-backwardslist=list(reversed(listofwords))
+#6. invert list of words
+backwardsList =list(reversed(listofWords))
 
-#join list using spaces
-print("".join(backwardslist))
+#7. join list using spaces 
+print(" ".join(backwardsList))
+      
+#8. create boolean asking of "python" is in list of words 
+pythonIncluded= ('python')
 
-#create boolean asking "if python"
-word_pythoninlist=True
-listofwords.find("python")
+if pythonIncluded in listofWords:
+    print('"Python" is found in your text')
 
-#make list into bigger list using each letter
-listofletters=list(userText.lower())
+#9. Make list into bigger list using ech letter
+listofLetters =list(userText.lower())
 
-#find 1st and last letter 
+#10. Find first and last letter of the list 
+firstL = [listofLetters[0]]
+lastL = [listofLetters[-1]]
+print (f'The first letter of the list is  {firstL},  and the last letter is {lastL}')
+#11. Print how many times the letter is found in the text 
+total1Letter= listofLetters.count(firstLetter)
+total2Letter= listofLetters.count(secondLetter)
+total3Letter= listofLetters.count(thirdLetter)
 
-#print how times the letter is found 
 
+
+choseLetters = { "inputs": { firstLetter: str(total1Letter), secondLetter: str(total2Letter), thirdLetter: str(total3Letter), } } 
+# Printing the occurrences of each letter for letter, 
+for letter, count in choseLetters["inputs"].items(): 
+    print(f'There are {count} occurrences of the letter "{letter}" in the text.')
 
---
+++ week_8/bellringer_boolean.py
@@ -0,0 +1,39 @@
+#1
+favlang=input("whats your fav progrzamming lang?")
+if favlang=='python':
+    print("python is great")
+else:
+    print("different choice")
+
+
+#2
+grade = int(input('What grade is being innputted?'))
+if grade >= 90:
+    print('the grade is a an A')
+elif grade >= 80 and grade < 90:
+    print('he grade is a b')
+elif grade >= 70 and grade < 80:
+    print ('the grade is a c')
+elif grade >= 60 and grade < 70:
+    print('the grade is a d')
+else:
+    print ('the grade is a f')
+
+
+
+#3
+user='admin'
+logged_in=True
+if user=="admin" and "logged in":
+    print("welcome")
+else:
+    print("not logged in")
+
+#4
+    list1=[1,2,3]
+    list2=[1,2,3]
+    print(list1 is list2)
+  
+
+
+    
